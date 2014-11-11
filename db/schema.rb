ff7a162c0845fe46b1f825aa699aa706321f7a48
Brandonmchu <brandon.mk.chu@gmail.com>
2014-11-10 20:34:47 -0500

    changes to postgres

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20141111005129) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "lunches", force: true do |t|
     t.string   "url"
     t.datetime "created_at"
